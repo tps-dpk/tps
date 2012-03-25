18e95f1120797e5c5c0ef7231dc4e391d5938d6a
adogan <online@ahmet-dogan.de>
2012-03-25 22:24:53 +0200

    phpinfo added

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+phpinfo();
 
 session_start();
 if (!empty($_SESSION['username'])) header( 'Location: ./loggedin.php' ) ;
@@ -8,15 +9,13 @@ echo <<<EOF
 "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>jQuery ThickBox Login Box</title>
-<script type="text/javascript" src="js/jquery-1.3.1.min.js"></script>
-<script type="text/javascript" src="js/thickBox-compressed.js"></script>
-<script type="text/javascript" src="js/login.js"></script></meta>
-<link rel="stylesheet" href="css/thickbox.css" type="text/css"
- media="screen" />
-<link rel="stylesheet" href="css/login.css" type="text/css"
-media="screen" />
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>jQuery ThickBox Login Box</title>
+	<script type="text/javascript" src="js/jquery-1.3.1.min.js"></script>
+	<script type="text/javascript" src="js/thickBox-compressed.js"></script>
+	<script type="text/javascript" src="js/login.js"></script></meta>
+	<link rel="stylesheet" href="css/thickbox.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="css/login.css" type="text/css" media="screen" />
 </head>
 <body>
 '<a href="login.htm?height=200&width=300" class="thickbox">Login</a>'
