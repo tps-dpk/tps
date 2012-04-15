d0005c7db02c716211c5697447a67435399381b2
adogan <online@ahmet-dogan.de>
2012-04-16 00:33:18 +0200

    .

--- js/login.js
+++ js/login.js
@@ -3,28 +3,26 @@ function login()
 var username = $("#username").val();
 var password = $("#password").val();
 
-if ((username == '') || (password == ''))
-{
+if ((username == '') || (password == '')) {
     $("#info").html('');
     $("#warning").html("Benutzername und Kennwort erforderlich !");
-} 
-else
-{
+} else {
    $.get("public/login.php?username="+username+"&password="+password, function(rc) {
-   if (rc == 0)
-{
-	$("#warning").html("Ihr Benutzer und/oder Kennwort sind falsch");
-	$("#info").html('');
+   if (rc == 0) {
+	   $("#warning").html("Ihr Benutzer und/oder Kennwort sind falsch");
+	   $("#info").html('');
+   } else if (rc == 1) {
+	   $("#info").html('');
+	   $("warning").html('');
+	   $("#loginDiv").html("Login erfolgreich");
+
+    	setTimeout("window.location.reload()",1000);
+    	setTimeout("tb_remove()",3000);
+    } else {
+    	$("#info").html("kein gueltige Rueckgabewert beim Login.php");
+    }
+  });
 }
-       else if (rc == 1)
-{
-$("#info").html('');
-$("warning").html('');
-$("#loginDiv").html("Login erfolgreich");
-setTimeout("window.location.reload()",1000);
-setTimeout("tb_remove()",3000);
-}
-});
-}
+
 return false;
 }
