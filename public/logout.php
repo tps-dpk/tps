1be97e16fab2ff3b47746073deebed4df37ffc31
adogan <online@ahmet-dogan.de>
2012-04-16 00:24:49 +0200

    .

--- public/logout.php
+++ public/logout.php
@@ -3,5 +3,5 @@ ini_set('display_errors', 'On');
 error_reporting(E_ALL | E_STRICT);
      session_start();
      session_destroy();
-     header('Location: ./index.php');
+     header('Location: ../index.php');
 ?>
