a2af0b65ead4d8fb7b4d27baf9f174942fda622b
adogan <online@ahmet-dogan.de>
2012-05-05 18:44:11 +0200

    new files

---
+++ inc/form.php
@@ -0,0 +1,6 @@
+<script language="javascript" src="YUI/2.9/build/yahoo/yahoo-min.js" type="text/javascript">
+</script>
+<script language="javascript" src="YUI/2.9/build/event/event-min.js" type="text/javascript">
+</script>
+<script language="javascript" src="scripts/jsvalidate.js" type="text/javascript">
+</script>
